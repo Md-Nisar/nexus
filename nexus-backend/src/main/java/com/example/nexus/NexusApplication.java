5be925361f6da490cca51c6ddfdc91ccf8fc1244
Md Nisar Ahmed <nisar.a@entomo.co>
2026-06-05 22:51:49 +0530

    Backend Port - 1000 & Frontend Port - 2000

--- nexus-backend/src/main/java/com/example/nexus/NexusApplication.java
+++ nexus-backend/src/main/java/com/example/nexus/NexusApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NexusBackendApplication {
+public class NexusApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NexusBackendApplication.class, args);
+		SpringApplication.run(NexusApplication.class, args);
 	}
 
 }
