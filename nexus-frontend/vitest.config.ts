63bc5f0bb0ea5b86ce8dc3de89dba102a3648445
Md Nisar Ahmed <nisar.a@entomo.co>
2026-08-12 15:25:51 +0530

    Sonar - Github Actions Coverage Analyis setup

--- nexus-frontend/vitest.config.ts
+++ nexus-frontend/vitest.config.ts
@@ -3,5 +3,8 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     testTimeout: 20000,
+    coverage: {
+      reporter: ['text', 'lcov', 'clover', 'html'],
+    },
   },
 });
